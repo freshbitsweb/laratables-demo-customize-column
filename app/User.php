2033eaa25860beb2ed0497227b0230788658f58b
Rajan <rajan@freshbits.in>
2019-11-21 16:18:42 +0530

    user data seed

--- app/User.php
+++ app/User.php
@@ -11,29 +11,9 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * Indicates if the model should be timestampe
      *
-     * @var array
+     * @var bool
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public $timestamps = false;
 }
